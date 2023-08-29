d3ecf5f244ba53c129f5b475fd20232f7f4e1f86
Christian Gördes <christian.goerdes@outlook.de>
2023-08-29 22:17:35 +0200

    added exception handling

---
+++ src/main/java/com/goerdes/security/exception/JwtExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.goerdes.security.exception;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class JwtExceptionHandler {
+    @ExceptionHandler(value = ExpiredJwtException.class)
+    public ResponseEntity<String> handleExpiredJwtException(ExpiredJwtException expiredJwtException) {
+        return new ResponseEntity<>("JWT expired", HttpStatus.BAD_REQUEST);
+    }
+}
