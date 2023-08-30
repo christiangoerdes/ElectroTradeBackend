4be403ec05dd2298bef823a8680dd584c492eb38
Christian Gördes <christian.goerdes@outlook.de>
2023-08-31 00:33:09 +0200

    added method to get the timestamps

--- src/main/java/com/goerdes/security/market/MarketService.java
+++ src/main/java/com/goerdes/security/market/MarketService.java
@@ -3,6 +3,9 @@ package com.goerdes.security.market;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -17,6 +20,20 @@ public class MarketService {
     }
 
     public StockList getAllStocks() {
-        return StockList.builder().stocks(marketRepo.findAll()).build();
+        return StockList.builder().stocks(marketRepo.findAll()).timestamps(getLast30DaysFormatted()).build();
+    }
+
+    private static List<String> getLast30DaysFormatted() {
+        List<String> formattedDates = new ArrayList<>();
+
+        LocalDate currentDate = LocalDate.now();
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("MMM d");
+
+        for (int i = 0; i < 30; i++) {
+            formattedDates.add(currentDate.format(dateFormatter).replace(".", ""));
+            currentDate = currentDate.minusDays(1);
+        }
+
+        return formattedDates;
     }
 }
