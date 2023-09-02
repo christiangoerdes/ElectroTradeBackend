5324f5f05a7a59dffab007add7714c514ed28c92
Christian Gördes <christian.goerdes@outlook.de>
2023-09-02 14:33:47 +0200

    optimized imports

--- src/main/java/com/goerdes/security/market/MarketEntity.java
+++ src/main/java/com/goerdes/security/market/MarketEntity.java
@@ -5,12 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Type;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 @Data
 @Builder
